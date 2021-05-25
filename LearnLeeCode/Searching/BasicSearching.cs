db621d3db0c4e3811da83a199037e6862381353e
kissinger chen <kchen@aurorasolartech.com>
2021-05-24 17:53:19 -0700

    Add binary search method

---
+++ LearnLeeCode/Searching/BasicSearching.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnLeeCode.Searching
+{
+    public class BasicSearching
+    {
+        public static int BinarySearch(int target, int[] source)
+        {
+            int min = 0;
+            int max = source.Length - 1;
+            while (min < max)
+            {
+                int middle = (min + max) / 2;
+                if (source[middle] == target)
+                {
+                    return middle;
+                }
+                else if (target < source[middle])
+                {
+                    max = middle - 1;
+                }
+                else
+                {
+                    min = middle + 1;
+                }
+            }
+            return -1; 
+        }
+    }
+}
---
+++ LearnLeeCodeTests/Searching/BasicSearchingTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnLeeCode.Searching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LearnLeeCodeTests;
+
+namespace LearnLeeCode.Searching.Tests
+{
+    [TestClass()]
+    public class BasicSearchingTests
+    {
+        [TestMethod()]
+        public void BinarySearchTest()
+        {
+            var input = RandomList.GetSortedRandomArray(11);
+            var resultIndex = BasicSearching.BinarySearch(5, input);
+        }
+    }
+}
